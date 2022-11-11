979a9bcb47ec5a2e4f0b8abf7cf112207c7ae748
funzter <funzter@mail.timesnake.de>
2022-11-11 20:31:15 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/game/survivalgames/chat/Plugin.java
+++ src/main/java/de/timesnake/game/survivalgames/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * game-survivalgames.main
+ * timesnake.game-survivalgames.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,11 +18,16 @@
 
 package de.timesnake.game.survivalgames.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.library.extension.util.chat.Plugin {
 
-    public static final Plugin SURVIVAL_GAMES = new Plugin("SurvivalGames", "GSG");
+    public static final Plugin SURVIVAL_GAMES = new Plugin("SurvivalGames", "GSG", LogHelper.getLogger("SurvivalGames", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
